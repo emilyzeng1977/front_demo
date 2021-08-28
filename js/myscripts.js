c35e969ffa1b475531449a2d4d27b3a98d65c52b
tniu <tniu@infomedia.com.au>
2021-08-28 10:21:12 +1000

    init

---
+++ js/myscripts.js
@@ -0,0 +1,7 @@
+function bulboff() {
+  document.getElementById('myImage').src='images/pic_bulboff.gif'
+}
+
+function bulbon() {
+  document.getElementById('myImage').src='images/pic_bulbon.gif'
+}
